08eb6dd26c6c1c58a3fc266d818edb4ac23bb486
Victor Toni <victor.toni@gmail.com>
2023-04-14 19:51:33 +0200

    Move creation code for random elements out of setup code of example

--- example/setup.ts
+++ example/setup.ts
@@ -1,57 +1,5 @@
 import { DEFAULTS, Selector, SelectionMode } from "../src/selector";
-
-//==============================================================================
-
-const margin = 3;
-
-const sizeClasses = [
-    {
-        name: "tiny",
-        size: 15
-    },
-    {
-        name: "small",
-        size: 30
-    },
-    {
-        name:  "regular",
-        size: 50
-    },
-    {
-        name: "big",
-        size: 75
-    }
-];
-
-//==============================================================================
-
-function generateRandomElements(parent: HTMLElement, columns: number, height: number) {
-    for (let i = 0; i < columns; i++) {
-        parent.appendChild(generateColumn(height));
-    }
-}
-
-function generateColumn(height: number): HTMLElement {
-    const column = document.createElement("div");
-    column.className = "column";
-
-    generateColumnElements(column, height);
-
-    return column;
-}
-
-function generateColumnElements(column: HTMLElement, height: number) {
-    let calculatedHeight = - margin;
-    while (calculatedHeight < height) {
-        const element = document.createElement("div");
-        const index = Math.floor(Math.random() * sizeClasses.length);
-
-        element.className = "selectable " + sizeClasses[index].name;
-        column.appendChild(element);
-
-        calculatedHeight += margin + sizeClasses[index].size;
-    }
-}
+import { generateRandomElements } from "./randomElements";
 
 //==============================================================================
 
