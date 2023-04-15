2ba8e8fc720cb91ee4edb1014afaa72912901cb2
Victor Toni <victor.toni@gmail.com>
2023-04-15 15:42:34 +0200

    Add main entry point for publishing

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { DEFAULTS, SelectionMarkMode, SelectionMode, Selector } from "./selector";
+import { htmlRandomUUID } from "./randomUUID";
+
+//==============================================================================
+
+export {
+    htmlRandomUUID,
+    DEFAULTS,
+    SelectionMarkMode,
+    SelectionMode,
+    Selector
+};
+
+//==============================================================================
